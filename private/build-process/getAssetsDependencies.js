523284855a1ca9b58624213ba053df08dd3d7903
run3er <run3er@gmail.com>
2017-03-28 15:08:22 +0100

    Made some minor improvement.
    * Added/Fixed getDependencies script cli argument passing.
    * Fixed all dependencies to exact versions.
    * Added ng-cloak to views (ng-view).

--- private/build-process/getAssetsDependencies.js
+++ private/build-process/getAssetsDependencies.js
@@ -11,72 +11,98 @@ var http = require('http');
 
 // App variables
 var configParam = "pm-assetDependencies";
-var package_jsonPath;
+var configFileName = "package.json";
+var configFilePathArg;
+var pathDelimiter = process.argv[1][0] === "/" ? "/" : "\\";
+var path;
 if (process.argv.length === 3) {
-	package_jsonPath = process.argv[2];
+	configFilePathArg = process.argv[2];
+	if (configFilePathArg[configFilePathArg.length - 1] === pathDelimiter) {
+		configFilePathArg.splice(configFilePathArg.length - 1, 1);
+	}
+	path = configFilePathArg;
+	fs.readFile(path + pathDelimiter + configFileName, 'utf8', function (err, data) {
+		if (err) {
+			fatalError();
+			return;				
+		}
+		onSuccess(data);
+	});
 } 
 else {
-	// Locating package.json file in the script's path hierarchy
-	console.log("Locating package.json in the script's path hierarchy ...");
-	var pathDelimiter = process.argv[1][0] === "/" ? "/" : "\\";
+	// Locating config file in the script's path hierarchy
+	console.log("Locating " + configFileName + " in the script's path hierarchy ...");
 	var parentFolders = process.argv[1].split(pathDelimiter);
 	parentFolders.splice(parentFolders.length - 1, 1);
-	var path = parentFolders.join(pathDelimiter);
-	var ix = 1;
+	path = parentFolders.join(pathDelimiter);
 	var pathHierarchy = [];
 
-	fs.readFile(path + pathDelimiter + 'package.json', 'utf8', function retrieveConfigFileContent(err, data) {
+	fs.readFile(path + pathDelimiter + configFileName, 'utf8', function retrieveConfigFileContent(err, data) {
 		if (err ) {
 			pathHierarchy.push(path + pathDelimiter);
-			if (parentFolders.length === 0 | (parentFolders.length === 1 && parentFolders[0] !== "")) {
-				console.log("Fatal error at script '" + process.argv[1] + "'.\n" + 
-							"Error getting '"+ configParam + "' from package.json. \n" + 
-							"Could not locate package.json file in the script's path hierarchy:");
-				for (var i = 0; i < pathHierarchy.length; i++) {
-					console.log(" " + pathHierarchy[i]);
-				}
-				if (process.argv.length !== 3) {
-					console.log("--Make sure to pass the location of the 'package.json' target file as " + 
-								"an argument if it is not located in the script's path hierarchy.");
-
-				}
-				process.exitCode = -1;
+			// Unix || Windows path-style checking
+			if (parentFolders.length === 0 || (parentFolders.length === 1 && parentFolders[0] !== "")) {
+				fatalError();
 				return;				
 			}
 			parentFolders.splice(parentFolders.length - 1, 1);
 			path = parentFolders.join(pathDelimiter);
-			fs.readFile(path + pathDelimiter + 'package.json', 'utf8', retrieveConfigFileContent);	
+			fs.readFile(path + pathDelimiter + configFileName, 'utf8', retrieveConfigFileContent);	
 			return;
 		}
-		console.log("Located package.json at '" + path + "'.\n");
+		onSuccess(data);
+	});		
+}
 
-		// Read asset dependencies list from package.json
-		console.log("\nDownloading assets (" + configParam + ") found in package.json.\n");
-		try {
-			var devDependencies = JSON.parse(data)[configParam];
-			for(var i = 0; i < devDependencies.length; i++) {
-				var assetsPath = path + pathDelimiter + devDependencies[i].assetsPath;
-				// Create path if doesn't exist
-				assetsPath
-				 .split('/')
-				 .reduce((path, folder) => {
-				   path += folder + '/';
-				   if (!fs.existsSync(path)){
-					 fs.mkdirSync(path);
-				   }
-				   return path;
-				 }, '');
-				// Download assets to specified path
-				for(var j = 0; j < devDependencies[i].assetsURL.length; j++) {
-					download_file_httpget(devDependencies[i].assetsURL[j], assetsPath);
-				}
+
+function fatalError() {
+	console.log("Fatal error at script '" + process.argv[1] + "'.\n" + 
+				"Error getting '"+ configParam + "' from " + configFileName + ".");
+	if (configFilePathArg) {
+		console.log("Could not locate " + configFileName + " file in the argument: " + configFilePathArg);
+	}
+	else {
+		console.log("Could not locate " + configFileName + " file in the script's path hierarchy:");
+		for (var i = 0; i < pathHierarchy.length; i++) {
+			console.log(" " + pathHierarchy[i]);
+		}
+	}
+	if (process.argv.length !== 3) {
+		console.log("--Make sure to pass the location of the '" + configFileName + "' target file as " + 
+					"an argument if it is not located in the script's path hierarchy.");
+	}
+	process.exitCode = -1;
+}
+
+
+function onSuccess(data) {
+	console.log("Located " + configFileName + " at '" + path + "'.\n");
+	// Read asset dependencies list from config file
+	console.log("\nDownloading assets (" + configParam + ") found in " + configFileName + ".\n");
+	try {
+		var devDependencies = JSON.parse(data)[configParam];
+		for(var i = 0; i < devDependencies.length; i++) {
+			var assetsPath = path + pathDelimiter + devDependencies[i].assetsPath;
+			// Create path if doesn't exist
+			assetsPath
+			 .split('/')
+			 .reduce((path, folder) => {
+			   path += folder + '/';
+			   if (!fs.existsSync(path)){
+				 fs.mkdirSync(path);
+			   }
+			   return path;
+			 }, '');
+			// Download assets to specified path
+			for(var j = 0; j < devDependencies[i].assetsURL.length; j++) {
+				download_file_httpget(devDependencies[i].assetsURL[j], assetsPath);
 			}
 		}
-		catch (e) {
-			console.log("Error at script '" + process.argv[1] + "': " + e.message);
-			process.exitCode = -1;
-		}
-	});		
+	}
+	catch (e) {
+		console.log("Error at script '" + process.argv[1] + "': " + e.message);
+		process.exitCode = -1;
+	}
 }
 
 
