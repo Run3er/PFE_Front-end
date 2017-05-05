0128842243dcfb1a062b0e5092223c1f45301e9d
run3er <run3er@gmail.com>
2017-05-05 17:26:58 +0100

    Added complex entries non-transactional addition logic.

--- private/js/services/DB.js
+++ private/js/services/DB.js
@@ -1,9 +1,14 @@
 
 angular.module('ProjMngmnt')
-    // Database layer mockup
+// Database layer mockup
     .service('DB', function ($q, $http) {
         // API server address
         var serverAddress = "http://localhost:9000";
+
+
+        // TODO: make transactional
+        // Transaction-unhandled entries (due to inter association with other entries)
+        var nonTransactionallyPersistedEntries = [ "action", "pendingIssue" ];
 
 
         // Tenant specific data
@@ -48,21 +53,30 @@ angular.module('ProjMngmnt')
                         });
                 },
                 add: function (entry) {
-                    var postBody;
+                    if (nonTransactionallyPersistedEntries.indexOf(entryProps.type) === -1) {
+                        // Use transactional bulk entry add-n-associate API (via nested path)
+                        var postBody = uriPrefix.length === 0 ? entry : [ entry ];
 
-                    if (uriPrefix.length === 0) {
-                        postBody = entry;
+                        return $http.post(serverAddress + "/" + uriPrefix + entriesUriName, postBody)
+                            .then(function (response) {
+                                var appendedEntryId = response.data[0];
+
+                                return appendedEntryId;
+                            });
                     }
                     else {
-                        postBody = [ entry ];
+                        // Two times (non-transactional) operation; first persist the entry itself
+                        return $http.post(serverAddress + "/" + entriesUriName, entry)
+                            .then(function (response) {
+                                var urlParts = response.data._links.self.href.split("/");
+                                var appendedEntryId = urlParts[urlParts.length - 1];
+                                var uriList = entriesUriName + "/" + appendedEntryId;
+
+                                // Now, associate the persisted entry with its parent resource
+                                return $http.post(serverAddress + "/" + uriPrefix + entriesUriName, uriList,
+                                        { headers: { "Content-Type": "text/uri-list" } });
+                            });
                     }
-
-                    return $http.post(serverAddress + "/" + uriPrefix + entriesUriName, postBody)
-                        .then(function (response) {
-                            var appendedEntryId = response.data[0];
-
-                            return appendedEntryId;
-                        });
                 },
                 update: function (entry) {
 
